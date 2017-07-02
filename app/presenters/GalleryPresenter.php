bb0f2432ac3ccc0a486bf644b3f1e9155d9bedc0
Chriskorinek <chriskorinek@seznam.cz>
2017-07-02 11:49:35 +0200

    Rozpracovaná galerie

---
+++ app/presenters/GalleryPresenter.php
@@ -0,0 +1,94 @@
+<?php
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI\Form;
+use Nette\Utils\Image;
+use Nette\Database\Table\Selection;
+
+
+class GalleryPresenter extends Nette\Application\UI\Presenter
+{
+    /** @var Nette\Database\Context */
+    private $database;
+
+    public function __construct(Nette\Database\Context $database)
+    {
+        $this->database = $database;
+    }
+
+    public function renderDefault()
+    {
+        $this->template->images = $this->database->table('images');
+    }
+    
+    public function createComponentUploadForm()
+    {
+        if (!$this->getUser()->isLoggedIn()) {
+            $this->error('Pro vytvoření, nebo editování příspěvku se musíte přihlásit.');
+        }
+        else{
+            $form = new Form;
+            $form->addUpload('image')
+            ->setRequired(TRUE)
+            ->addRule(Form::IMAGE, 'Avatar musí být JPEG, PNG nebo GIF.');
+            $form->addText('title');
+            $form->addText('info');
+            
+            $form->addSubmit('login', 'Nahrát fotografii');
+            $form->onSuccess[] = [$this, 'uploadFormSucceeded'];
+            return $form;
+        }
+    }
+    
+    public function renderMyGallery()
+    {
+        if (!$this->getUser()->isLoggedIn()) {
+            $this->error('Pro prohlížení osobních fotografií se potřebujete přihlásit.');
+        }
+        else{
+            $this->template->images = $this->database->table('images')->where('user_id', $this->getUser()->id);
+        }
+    }
+    
+    public function renderShowImage($id)
+    {
+        $image = $this->database->table("images")->get($id);
+        if(!$image){$this->error('Tato stránka neexistuje');}
+        else{
+            $this->template->image = $image;
+        }
+    }
+    
+    public function actionDelete($id)
+    {
+        $image = $this->database->table("images")->get($id);
+        if($image->user_id == $this->getUser()->id){
+            unlink('userImages/'.$image->path.'.jpg');
+            $image->delete();
+            $this->redirect("my-gallery");
+        }
+        else{
+            $this->error("Nejste oprávněny toto udělat");
+        }
+    }
+    
+    public function uploadFormSucceeded(Form $form, $values)
+    {
+        $id_name = uniqid();
+        $image = $values->image;
+        unset($values['image']);
+        $values->user_id = $this->getUser()->id;
+        $values->path = $id_name;
+        $this->database->table('images')->insert($values);
+        
+        $image = Image::fromFile($image);
+        $image->save('userImages/'.$id_name.'.jpg');
+        $this->flashMessage('Obrázek byl úspěšně nahrán');
+        $this->redirect('Homepage:');
+    }
+    
+    public function check()
+    {
+    }
+}
