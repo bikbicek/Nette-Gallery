09ae12bf1dce1e9ac4f54b382a14f6701a75f05d
bob <bob@debian>
2017-07-14 16:56:38 +0100

    Zpracování zobrazování samostatných obrázků(data, úprava)

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -7,17 +7,6 @@ use Nette;
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
-    private $database;
-    
-    public function __construct(Nette\Database\Context $database)
-    {
-        $this->database = $database;
-    }
-    
-    public function renderDefault()
-    {
-        $this->template->posts = $this->database->table('posts');
-    }
     
     public function renderGalery(){}
 }
